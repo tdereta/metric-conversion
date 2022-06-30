1bea0aacdfc112c7ea3cdb60690999812ee07867
tdereta <tdereta@gmail.com>
2022-06-30 16:13:03 +0200

    Add 'error' if entered letters instead numbers.

--- index.js
+++ index.js
@@ -9,27 +9,22 @@ let convertBtn = document.getElementById("convert-btn")
 let lengthEl = document.getElementById("length-el")
 let volumeEl = document.getElementById("volume-el")
 let massEl = document.getElementById("mass-el")
+let errorEl = document.getElementById("error")
 
-const meterToFeet =  (3.281).toFixed(3)
-const literToGallon =  (0.264).toFixed(3)
-const kiloToPound =  (2.204).toFixed(3)
-
-// const meter = (value * (1/3.281)).toFixed(3);
-// const feet = (value * 3.281).toFixed(3);
-// const litre = (value * (1/0.264)).toFixed(3);
-// const gallon = (value * 0.264).toFixed(3);
-// const kilogram = (value * (1/2.204)).toFixed(3);
-// const pound = (value * 2.204).toFixed(3);
+const meterToFeet =  3.281
+const literToGallon =  0.264
+const kiloToPound =  2.204
 
 convertBtn.addEventListener("click", function() {
     let inputValue = inputEl.value
 
-    lengthEl.textContent = `${inputValue} meters = ${inputValue * meterToFeet} feet | 
-                           ${inputValue} feet = ${inputValue / meterToFeet} meters`
+    if (!isNaN(inputValue)) {
+        lengthEl.textContent = `${inputValue} meters = ${(inputValue * meterToFeet).toFixed(3)} feet | ${inputValue} feet = ${(inputValue / meterToFeet).toFixed(3)} meters`
 
-    volumeEl.textContent = `${inputValue} liters = ${inputValue * literToGallon} gallons |
-                           ${inputValue} gallons = ${inputValue / literToGallon} liters`
+        volumeEl.textContent = `${inputValue} liters = ${(inputValue * literToGallon).toFixed(3)} gallons | ${inputValue} gallons = ${(inputValue / literToGallon).toFixed(3)} liters`
 
-    massEl.textContent = `${inputValue} kilograms = ${inputValue * kiloToPound} pounds |
-                         ${inputValue} pounds = ${inputValue / kiloToPound} kilograms`
+        massEl.textContent = `${inputValue} kilograms = ${(inputValue * kiloToPound).toFixed(3)} pounds | ${inputValue} pounds = ${(inputValue / kiloToPound).toFixed(3)} kilograms`
+    } else {
+        errorEl.textContent = `You must enter a number, silly!`
+    }
 })
